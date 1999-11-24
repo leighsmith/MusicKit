7d38a49e79e0c1bd54c3c38984e10981d0fed64a
Leigh Smith <leigh@tomandandy.com>
1999-11-24 16:14:51 -0500

    This commit was manufactured by cvs2svn to create branch 'tomandandy'.

---
+++ MusicKit/Utilities/ScorefileUtils/ScoreInspector/ScoreInspector.h
@@ -0,0 +1,14 @@
+#ifndef __MK_SCOREInspector_H___
+#define __MK_SCOREInspector_H___
+
+#import <AppKit/AppKit.h>
+
+@interface ScoreInspector:WMInspector
+{
+}
+
+- openInEdit:sender;
+- play:sender;
+
+@end
+#endif
