be5a5ab0c57d3ae557492c2f9a03b68d1a07e818
Leigh Smith <leigh@tomandandy.com>
2000-05-30 11:34:18 -0500

    Fixed comments and added a free fudge method

--- MusicKit/Frameworks/SndKit/SndDisplayData.h
+++ MusicKit/Frameworks/SndKit/SndDisplayData.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 LEGAL:
-This example application and all source code supplied with it, are Copyright Stephen Brandon and the University of Glasgow, 1999. You are free to use the source code for any purpose, including commercial applications, as long as you reproduce this notice on all such software.
+This framework and all source code supplied with it, except where specified, are Copyright Stephen Brandon and the University of Glasgow, 1999. You are free to use the source code for any purpose, including commercial applications, as long as you reproduce this notice on all such software.
 
 Software production is complex and we cannot warrant that the Software will be error free.  Further, we will not be liable to you if the Software is not fit for the purpose for which you acquired it, or of satisfactory quality. 
 
@@ -11,6 +11,7 @@ If a court finds that we are liable for death or personal injury caused by our n
 WE SHALL HAVE NO LIABILITY TO YOU FOR LOSS OF PROFITS, LOSS OF CONTRACTS, LOSS OF DATA, LOSS OF GOODWILL, OR WORK STOPPAGE, WHICH MAY ARISE FROM YOUR POSSESSION OR USE OF THE SOFTWARE OR ASSOCIATED DOCUMENTATION.  WE SHALL HAVE NO LIABILITY IN RESPECT OF ANY USE OF THE SOFTWARE OR THE ASSOCIATED DOCUMENTATION WHERE SUCH USE IS NOT IN COMPLIANCE WITH THE TERMS AND CONDITIONS OF THIS AGREEMENT.
 
 ******************************************************************************/
+
 
 #import <AppKit/AppKit.h>
 
