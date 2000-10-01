52e2bf11ecd5094af7bd95ff61eb6bba2e16d043
Leigh Smith <leigh@tomandandy.com>
2000-10-01 01:55:48 -0500

    Properly typed function prototypes, added _MKKeyNumPrintfunc prototype.

--- MusicKit/Frameworks/MusicKit/TuningSystemPrivate.h
+++ MusicKit/Frameworks/MusicKit/TuningSystemPrivate.h
@@ -7,6 +7,9 @@
 Modification history:
 
   $Log$
+  Revision 1.3  2000/10/01 06:55:48  leigh
+  Properly typed function prototypes, added _MKKeyNumPrintfunc prototype.
+
   Revision 1.2  1999/07/29 01:25:58  leigh
   Added Win32 compatibility, CVS logs, SBs changes
 
@@ -15,13 +18,14 @@ Modification history:
 #ifndef __MK__TuningSystem_H___
 #define __MK__TuningSystem_H___
 
-/* Writing frequencies */
-extern BOOL _MKFreqPrintfunc();
-
 /* Tuning system functions */
-extern void _MKTuningSystemInit();
+extern void _MKTuningSystemInit(void);
 extern int _MKFindPitchVar(id aVar);
 
+/* Writing frequencies */
+extern BOOL _MKFreqPrintfunc(_MKParameter *param, NSMutableData *aStream, _MKScoreOutStruct *p);
 
+// Writing keynumbers
+extern BOOL _MKKeyNumPrintfunc(_MKParameter *param, NSMutableData *aStream, _MKScoreOutStruct *p);
 
 #endif
