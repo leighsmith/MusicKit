9b4e1db902c22dce30d478afd1c8c98cc92d05c4
Leigh Smith <leigh@tomandandy.com>
2000-11-25 18:01:40 -0500

    Enforced ivar privacy, removed -releasePartRecorders, doco cleanup, correctly typed partRecorderClass to Class

--- MusicKit/Frameworks/MusicKit/MKScoreRecorder.h
+++ MusicKit/Frameworks/MusicKit/MKScoreRecorder.h
@@ -26,9 +26,13 @@
   Copyright (c) 1988-1992, NeXT Computer, Inc.
   Portions Copyright (c) 1994 NeXT Computer, Inc. and reproduced under license from NeXT
   Portions Copyright (c) 1994 Stanford University
+  Portions Copyright (c) 1999-2000, The MusicKit Project.
 */
 /*
   $Log$
+  Revision 1.5  2000/11/25 23:01:40  leigh
+  Enforced ivar privacy, removed -releasePartRecorders, doco cleanup, correctly typed partRecorderClass to Class
+
   Revision 1.4  2000/06/09 02:56:02  leigh
   Comment cleanup and correct typing of ivars
 
@@ -47,14 +51,14 @@
 
 @interface MKScoreRecorder : NSObject
 {
-    NSMutableArray *partRecorders; /* The object's Set of MKPartRecorders. */
-    MKScore *score; /* The object's MKScore. */
-    MKTimeUnit timeUnit; /* Unit the object's PartRecorders use to 
-                            measure time; one of MK_second or MK_beat. */
-    id partRecorderClass; /* The PartRecorder subclass used. */
+    NSMutableArray *partRecorders; /* The object's collection of MKPartRecorders. */
+    MKScore *score;                /* The object's MKScore. */
+    MKTimeUnit timeUnit;           // Unit the object's MKPartRecorders use to 
+                                   // measure time; one of MK_second or MK_beat.
+    Class partRecorderClass;       /* The MKPartRecorder subclass used. */
     BOOL compensatesDeltaT;
 
-    /* The following is for internal use only */
+@protected
     BOOL _noteSeen;
 }
 
@@ -66,54 +70,41 @@
   * The return value is ignored.  
   */
 
-//- (void)initialize;
- /* Obsolete.
-  */
-
-- setScore:aScore; 
+- setScore: (MKScore *) aScore; 
  /* 
-  * Removes and frees the receiver's current PartRecorders, sets its Score
-  * to aScore, and then creates and adds a PartRecorder for each Part in
-  * the Score.  Subsequent changes to aScore (adding or removing Parts)
+  * Removes and frees the receiver's current MKPartRecorders, sets its MKScore
+  * to aScore, and then creates and adds a MKPartRecorder for each MKPart in
+  * the MKScore.  Subsequent changes to aScore (adding or removing Parts)
   * aren't seen by the receiver.  If the receiver is in performance, this
   * does nothing and returns nil, otherwise it returns the receiver.
   * 
-  * If you want to set the Score without freeing the current PartRecorders you
+  * If you want to set the MKScore without freeing the current MKPartRecorders you
   * should send removePartRecorders before invoking this method; the
-  * PartRecorders are then removed but not freed.  
+  * MKPartRecorders are then removed but not freed.  
   */
 
-- score; 
- /* Returns the receiver's Score. */
+- (MKScore *) score; 
+ /* Returns the receiver's MKScore. */
 
 - copyWithZone:(NSZone *)zone; 
  /* 
-  * Creates and returns a ScoreRecorder as a copy of the receiver.  The
-  * new object has the same Score as the receiver, but contains its own
-  * set of PartRecorders.
+  * Creates and returns a MKScoreRecorder as a copy of the receiver.  The
+  * new object has the same MKScore as the receiver, but contains its own
+  * set of MKPartRecorders.
   */
 
 -copy;
  /* Same as [self copyFromZone:[self zone]]; */
 
-- releasePartRecorders;
- /* 
-  * If the receiver is in performance, returns nil and does nothing. Otherwise,
-  * Frees the receiver's PartRecorders and sets its Score to nil.
-  * Returns the receiver.  
-  */
-
 - removePartRecorders; 
  /* 
-  * Removes the receiver's PartRecorders and sets its Score to nil.  (The
-  * PartRecorder objects aren't freed.)  Returns the receiver.  
+  * Removes the receiver's MKPartRecorders and sets its MKScore to nil.  (The
+  * MKPartRecorder objects aren't freed.)  Returns the receiver.  
   */
 
 - (void)dealloc; 
  /* 
-  * Frees the receiver and its PartRecorders.  If you want to free the
-  * receiver only, send removePartRecorders to the receiver before
-  * invoking this method.
+  * Frees the receiver and its MKPartRecorders.  You never directly invoke this method. 
   */
 
 - (MKTimeUnit)timeUnit;
@@ -123,14 +114,14 @@
  /* 
   * Sets the receiver's time unit to aTimeUnit, one of MK_beat and
   * MK_second, and forwards the setTimeUnit:aTimeUnit message to the
-  * receiver's PartRecorders.  If the receiver is in performance, this
+  * receiver's MKPartRecorders.  If the receiver is in performance, this
   * does nothing and returns nil.  Otherwise returns the receiver.
   */
 
 - partRecorders; 
  /* 
-  * Returns a List object that contains the receiver's PartRecorders.
-  * It's the sender's responsibility to free the List.
+  * Returns a NSArray object that contains the receiver's MKPartRecorders.
+  * It's the sender's responsibility to free the NSArray.
   */
 
 - (BOOL )inPerformance;
@@ -141,7 +132,7 @@
 - firstNote:aNote; 
  /* 
   * You never invoke this method; it's invoked automatically when the
-  * first Note is received by any of the receiver's PartRecorders.  The
+  * first MKNote is received by any of the receiver's MKPartRecorders.  The
   * default does nothing; a subclass can implement this method for
   * performance initialization.  The returns value is ignored.
   */
@@ -156,25 +147,25 @@
 
 - noteReceivers; 
  /* 
-  * Returns a List object that contains the receiver's NoteReceivers.  You
-  * must free the List yourself when you're done with it.
+  * Returns a NSArray object that contains the receiver's MKNoteReceivers.  You
+  * must free the NSArray yourself when you're done with it.
   */
 
 - partRecorderForPart:aPart; 
  /* 
-  * Returns the receiver's PartRecorder for aPart, or nil if not found.
+  * Returns the receiver's MKPartRecorder for aPart, or nil if not found.
   */ 
 
 -setPartRecorderClass:aPartRecorderSubclass;
- /* Normally, ScoreRecorders create instances of the PartRecorder class.
-   This method allows you to specify that instances of some PartRecorder
+ /* Normally, ScoreRecorders create instances of the MKPartRecorder class.
+   This method allows you to specify that instances of some MKPartRecorder
    subclass be created instead. If aPartRecorderSubclass is not 
-   a subclass of PartRecorder (or PartRecorder itself), this method has 
+   a subclass of MKPartRecorder (or MKPartRecorder itself), this method has 
    no effect and returns nil. Otherwise, it returns self.
   */
 -partRecorderClass;
- /* Returns the class used for PartRecorders, as set by 
-   setPartRecorderClass:. The default is PartRecorder itself. */
+ /* Returns the class used for MKPartRecorders, as set by 
+   setPartRecorderClass:. The default is MKPartRecorder itself. */
 
 - (void)encodeWithCoder:(NSCoder *)aCoder;
   /* 
@@ -189,7 +180,7 @@
      Should be invoked with NXReadObject(). 
      */
 
-+ new; 
+//+ new; 
  /* Obsolete */
    
 @end
