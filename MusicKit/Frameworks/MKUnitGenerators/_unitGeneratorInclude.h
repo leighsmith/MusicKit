ce8a832cfbf536818d9a70cdc310b32431694fd9
Leigh Smith <leigh@tomandandy.com>
2001-02-10 22:16:42 -0500

    This commit was manufactured by cvs2svn to create branch 'CoreAudio'.

--- MusicKit/Frameworks/MKUnitGenerators/_unitGeneratorInclude.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MK__unitGeneratorInclude_H___
-#define __MK__unitGeneratorInclude_H___
-/* Copyright 1988-1992, NeXT Inc.  All rights reserved. */
-
-#define _MK_NOSET MAXDOUBLE
-#define TWO_TO_M_23 0.00000011920928955078125
-
-#define _MK_UGOPTIMIZE 1  /* Set to 0 and recompile to disable optimization */
-#endif
