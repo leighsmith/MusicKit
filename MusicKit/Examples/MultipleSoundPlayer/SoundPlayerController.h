bb79b8114deec1aae0288d0bacc7dd9167699903
Leigh Smith <leigh@tomandandy.com>
2001-03-02 21:55:01 -0500

    Converted to be a SndKit example, replacing NSSound with Snd

--- MusicKit/Examples/MultipleSoundPlayer/SoundPlayerController.h
+++ MusicKit/Examples/MultipleSoundPlayer/SoundPlayerController.h
@@ -1,13 +1,28 @@
+/*
+  $Id$
+
+  Description:
+    Very simple sound player application, demonstrating playing multiple sounds,
+    and the use of Snd delegates.
+    
+  Original Author: Leigh Smith, <leigh@tomandandy.com>, tomandandy music inc.
+
+  2-Mar-2001, Copyright (c) 2001 tomandandy music inc. All rights reserved.
+
+  Permission is granted to use and modify this code for commercial and non-commercial
+  purposes so long as the author attribution and copyright messages remain intact and
+  accompany all relevant code.
+*/
 #import <AppKit/AppKit.h>
+#import <SndKit/SndKit.h>
 
 @interface SoundPlayerController : NSObject
 {
     IBOutlet id soundFileNameTextBox;
-    NSArray *filesToPlay;
     IBOutlet id playButton;
+    NSArray *filesToPlay;
 }
 
-- init;
-- (void)chooseSoundFile:(id)sender;
-- (void)playSound:(id)sender;
+- (void) chooseSoundFile: (id) sender;
+- (void) playSound: (id) sender;
 @end
--- MusicKit/Examples/MultipleSoundPlayer/SoundPlayerController.m
+++ MusicKit/Examples/MultipleSoundPlayer/SoundPlayerController.m
@@ -1,16 +1,27 @@
+/*
+  $Id$
+
+  Description:
+    Very simple sound player application, demonstrating playing multiple sounds,
+    and the use of Snd delegates.
+    
+  Original Author: Leigh Smith, <leigh@tomandandy.com>, tomandandy music inc.
+
+  2-Mar-2001, Copyright (c) 2001 tomandandy music inc. All rights reserved.
+
+  Permission is granted to use and modify this code for commercial and non-commercial
+  purposes so long as the author attribution and copyright messages remain intact and
+  accompany all relevant code.
+*/
+
 #import "SoundPlayerController.h"
 
 @implementation SoundPlayerController
 
-- init
-{
-    return self;
-}
-
-- (void)chooseSoundFile:(id)sender
+- (void) chooseSoundFile: (id) sender
 {
     int result;
-    NSArray *fileTypes = [NSSound soundUnfilteredFileTypes];
+    NSArray *fileTypes = [Snd soundFileExtensions];
     NSOpenPanel *oPanel = [NSOpenPanel openPanel];
 
     NSLog(@"Accepting %@\n", fileTypes);
@@ -29,53 +40,37 @@
 
 }
 
-- (void)playSoundOld:(id)sender
+- (void) playSound: (id) sender
 {
-    NSSound *sound;
+    Snd *sound;
     int i, count = [filesToPlay count];
 
     NSLog(@"reading %@\n", filesToPlay);
     for (i=0; i<count; i++) {
         NSString *soundFileName = [filesToPlay objectAtIndex:i];
-        sound = [[NSSound alloc] initWithContentsOfFile: soundFileName byReference:NO];
+        sound = [[Snd alloc] initFromSoundfile: soundFileName];
         if(sound != nil) {
             NSLog(@"playing %@\n", soundFileName);
             [sound setDelegate: self];
+            [sound setName: soundFileName];
             [sound play];
         }
     }
 }
 
-
-- (void)playSound:(id)sender
+- (void) willPlay: (Snd *) sound duringPerformance: (SndPerformance *) performance
 {
-    NSSound *sound1;
-    NSSound *sound2;
-    NSString *soundFileName = [filesToPlay objectAtIndex:0];
-
-    NSLog(@"reading %d file %@\n", [filesToPlay count], filesToPlay);
-    sound1 = [[NSSound alloc] initWithContentsOfFile: soundFileName byReference:NO];
-    if(sound1 != nil) {
-        NSLog(@"playing %@\n", soundFileName);
-        [sound1 setDelegate: self];
-        [sound1 play];
-    }
-    if([filesToPlay count] > 1) {
-        NSString *soundFileName2 = [filesToPlay objectAtIndex:1];
-
-        sound2 = [[NSSound alloc] initWithContentsOfFile: soundFileName2 byReference:NO];
-        if(sound2 != nil) {
-            NSLog(@"playing %@\n", soundFileName2);
-            [sound2 setDelegate: self];
-            [sound2 play];
-        }
-    }
+    NSLog(@"will begin playing sound named %@\n", [sound name]);
 }
 
-- (void) sound:(NSSound *) sound didFinishPlaying:(BOOL)aBool
+- (void) didPlay: (Snd *) sound duringPerformance: (SndPerformance *) performance
 {
-    NSLog(@"did finish playing %d sound named %@\n", aBool, [sound name]);
+    NSLog(@"did finish playing sound named %@\n", [sound name]);
 }
 
+- (void) hadError: (Snd *) sound
+{
+    NSLog(@"had error playing sound named %@\n", [sound name]);
+}
 
 @end
