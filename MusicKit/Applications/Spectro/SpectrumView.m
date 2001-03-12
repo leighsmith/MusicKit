b7369d373a79162e92f551d0155a663672f16ff4
Leigh Smith <leigh@tomandandy.com>
2001-03-11 22:21:26 -0500

    Corrected vertical grid line placement

--- MusicKit/Applications/Spectro/SpectrumView.m
+++ MusicKit/Applications/Spectro/SpectrumView.m
@@ -4,9 +4,9 @@
 #import <AppKit/AppKit.h>
 #import <AppKit/NSColor.h>
 
-//#import <AppKit/psopsOpenStep.h>
-
-#define WIDTH    [self bounds].size.width		/* Width of view in pixels */
+#define SPECTRUM_WIDTH [self bounds].size.width		/* Width of view in pixels */
+#define VERT_GRID_VIEWABLE 4				/* number of vertical grid lines we want in the window */
+#define HORIZ_GRID_VIEWABLE 4				/* number of horizontal grid lines we want in the window */
 
 @implementation SpectrumView
 
@@ -19,22 +19,14 @@
     draw = NO;
     frames = NO;
     cursorPixel = 0;
-
-    spectrumColor = [StringToColor(
-            [[NSUserDefaults standardUserDefaults] objectForKey:@"SpectrumColor"]) retain];
+    [self setColors];
     
-    cursorColor = [StringToColor(
-                                    [[NSUserDefaults standardUserDefaults] objectForKey:@"CursorColor"]) retain];
-    
-    gridColor = [StringToColor(
-                                [[NSUserDefaults standardUserDefaults] objectForKey:@"GridColor"]) retain];
-
     return self;
 }
 
 - (void)setDelegate:(id)anObject
 {
-	delegate = anObject;
+    delegate = anObject;
 }
 
 - delegate
@@ -97,9 +89,9 @@
     NSBezierPath *horizLinePath;	/* path for horizontal lines */
     NSBezierPath *vertLinePath;		/* path for vertical lines */
     int i;
-    double xstep, ymax, xmax, xstart;
+    double xstep, ymax, gridSpacing;
     float height, width;
-    NSRect aRect;
+    NSRect viewable;
     NSPoint minCursor, maxCursor, horizPointLeft, horizPointRight, vertPointMin, vertPointMax;
 
     /* First, erase what we have now. */
@@ -108,46 +100,46 @@
         return;
 
     /* Set up the view width and other factors. */
-    cursorPixel = (int) 0.5 + cursorPixel * (length+1) / (WIDTH * dataFactor);
+    cursorPixel = (int) 0.5 + cursorPixel * (length+1) / (SPECTRUM_WIDTH * dataFactor);
     height = [self bounds].size.height;
     width = length / dataFactor;
     [self setFrameSize: NSMakeSize(width, height)];
 
     ymax = [self bounds].size.height;
     xstep = 1.0 / dataFactor;
-    xmax = WIDTH;
 
     // Fill the horizontal line path with the coordinates for the
     // horizontal lines.
     horizLinePath = [NSBezierPath bezierPath];
     [gridColor set];
-    for (i = 1; i < 4; i++) {
+    for (i = 1; i < HORIZ_GRID_VIEWABLE; i++) {
         horizPointLeft.x = 0.0;				/* X coord */
-        horizPointLeft.y = ymax * 0.25 * i;		/* Y coord */
+        horizPointLeft.y = ymax / HORIZ_GRID_VIEWABLE * i; /* Y coord */
         [horizLinePath moveToPoint: horizPointLeft];
 
-        horizPointRight.x = xmax;			/* X coord */
-        horizPointRight.y = ymax * 0.25 * i;		/* Y coord */
+        horizPointRight.x = SPECTRUM_WIDTH;		/* X coord */
+        horizPointRight.y = horizPointLeft.y;		/* Y coord */
         [horizLinePath lineToPoint: horizPointRight];
     }
+    [horizLinePath stroke];
 
     // Get the visible frame bounds, and fill the vertLinePath
-    // array with the coordinates for the vertical lines.
-    aRect = [(NSClipView *) [self superview] documentVisibleRect];
+    // with the coordinates for the vertical lines such that 
+    // VERT_GRID_VIEWABLE number of vertical grid lines are displayed.
+    viewable = [(NSClipView *) [self superview] documentVisibleRect];
     vertLinePath = [NSBezierPath bezierPath];
     [gridColor set];
-    xmax = aRect.size.width;
-    xstart = aRect.origin.x;
-    for (i = 1; i < 4; i++) {
-        vertPointMin.x = xstart + xmax * 0.25 * i;	/* X coord */
+    gridSpacing = viewable.size.width / VERT_GRID_VIEWABLE;
+    
+    for (i = 1; i < SPECTRUM_WIDTH / gridSpacing; i++) {
+        vertPointMin.x = gridSpacing * i;		/* X coord */
         vertPointMin.y = 0.0;				/* Y coord */
         [vertLinePath moveToPoint: vertPointMin];
 
-        vertPointMax.x = xstart + xmax * 0.25 * i;	/* X coord */
+        vertPointMax.x = vertPointMin.x;		/* X coord */
         vertPointMax.y = ymax;				/* Y coord */
-        [vertLinePath moveToPoint: vertPointMax];
+        [vertLinePath lineToPoint: vertPointMax];
     }
-    [horizLinePath stroke];
     [vertLinePath stroke];
     
     /* Create the cursor path. */
@@ -204,39 +196,34 @@
  * update the cursor as necessary and send a cursorMoved: message to
  * the delegate.
  */    
-	do {
-		p = [event locationInWindow];
-		p = [self convertPoint:p fromView:nil];
-		if (p.x < 0.0)
-			p.x = 0.0;
-		if (p.x > WIDTH)
-			p.x = WIDTH;
-		if (cursorPixel != p.x) {
+    do {
+        p = [event locationInWindow];
+        p = [self convertPoint:p fromView:nil];
+        if (p.x < 0.0)
+            p.x = 0.0;
+        if (p.x > SPECTRUM_WIDTH)
+            p.x = SPECTRUM_WIDTH;
+        if (cursorPixel != p.x) {
+            cursorPixel = p.x;
+            [self setNeedsDisplay:YES];
+                
+            if (delegate && [delegate respondsToSelector:@selector(cursorMoved:)])
+                [delegate cursorMoved:self];
+        }
+        event = [[self window] nextEventMatchingMask:MOVE_MASK];
+    } while ([event type] != NSLeftMouseUp);
 
-			cursorPixel = p.x;
-                    [self setNeedsDisplay:YES];
-			
-			if (delegate && 
-				[delegate respondsToSelector:@selector(cursorMoved:)])
-				[delegate cursorMoved:self];
-		}
-		event = [[self window] nextEventMatchingMask:MOVE_MASK];
-	} while ([event type] != NSLeftMouseUp);
-
-//	[[self window] setEventMask:oldMask];
-        [[self window] setAcceptsMouseMovedEvents:NO];
+//  [[self window] setEventMask:oldMask];
+    [[self window] setAcceptsMouseMovedEvents:NO];
 }
 
 - setColors
 {
-    spectrumColor = [StringToColor(
-                                   [[NSUserDefaults standardUserDefaults] objectForKey:@"SpectrumColor"]) retain];
+    spectrumColor = [StringToColor([[NSUserDefaults standardUserDefaults] objectForKey:@"SpectrumColor"]) retain];
 	
-    cursorColor = [StringToColor(
-                                 [[NSUserDefaults standardUserDefaults] objectForKey:@"CursorColor"]) retain];
+    cursorColor = [StringToColor([[NSUserDefaults standardUserDefaults] objectForKey:@"CursorColor"]) retain];
 	
-    gridColor = [StringToColor(
-                               [[NSUserDefaults standardUserDefaults] objectForKey:@"GridColor"]) retain];
+    gridColor = [StringToColor([[NSUserDefaults standardUserDefaults] objectForKey:@"GridColor"]) retain];
 
     [self setNeedsDisplay:YES];
 	
