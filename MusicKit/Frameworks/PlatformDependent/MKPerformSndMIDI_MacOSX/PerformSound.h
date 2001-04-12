4536c57654ce486662eb6dc39868999f771ae5ba
Leigh Smith <leigh@leighsmith.com>
2001-04-11 19:33:26 -0500

    First draft of HeaderDoc descriptions, much still to be done

--- MusicKit/Frameworks/PlatformDependent/MKPerformSndMIDI_MacOSX/PerformSound.h
+++ MusicKit/Frameworks/PlatformDependent/MKPerformSndMIDI_MacOSX/PerformSound.h
@@ -15,6 +15,9 @@
 */
 /*
   $Log$
+  Revision 1.7  2001/04/12 00:33:26  leighsmith
+  First draft of HeaderDoc descriptions, much still to be done
+
   Revision 1.6  2001/03/08 18:43:29  leigh
   Cleanup of includes
 
@@ -34,7 +37,19 @@
   Initial Release
 
 */
+/*!
+    @header PerformSound
+    
+    This is basically a bare-bones duplicate of NeXT/Apples' performsound module functionality.
+    It provides sound playback and recording, in either streaming (preferred) or single sound
+    operation (where the operating system lacks streaming buffers).
+    It draws inspiration (only) from Steinberg's ASIO.
+*/
 
+/*!
+    @defined PERFORM_API
+    @discussion This allows us to introduce anything necessary for library declarations, namely for Windows. Unused in MacOS X.
+*/
 #define PERFORM_API 
 
 #include <objc/objc.h> // for BOOL
@@ -45,58 +60,220 @@
 extern "C" {
 #endif 
 
+/*!
+    @typedef SNDNotificationFun
+    @param s
+    @param tag
+    @param err
+    @result
+*/
 typedef int (*SNDNotificationFun)(SndSoundStruct *s, int tag, int err);
+
+/*!
+    @typedef SNDStreamBuffer
+    @abstract Describes the format and the data in a limited length buffer used operating a stream.
+    @field streamFormat The format describing sample rate, number of channels etc. The field offset
+                        is not used since the streamData pointer can be used to refer to non-contiguous
+                        data.
+    @field streamData A pointer to the data itself. 
+*/
 typedef struct SNDStreamBuffer {
     SndSoundStruct streamFormat;
     void *streamData;
 } SNDStreamBuffer;
 
-// in and out are with respect to the audio hardware, i.e out == play
+/*!
+    @typedef SNDStreamProcessor
+    @discussion The descriptors "in" and "out" are with respect to the audio hardware, i.e out == play
+    @param sampleTime
+    @param inStream
+    @param outStream
+    @param userData
+*/
 typedef void (*SNDStreamProcessor)(double sampleTime, SNDStreamBuffer *inStream, SNDStreamBuffer *outStream, void *userData);
 
+/*!
+    @defined SND_NULL_FUN
+    @discussion Indicates no function is to be called.
+*/
 #define SND_NULL_FUN ((SNDNotificationFun)0)
 
+/*!
+    @function       SNDInit
+    @abstract       Initialise the Sound hardware.
+    @param          guessTheDevice
+                        Indicates whether to guess the device to select using the system default.
+    @discussion     The <tt>guessTheDevice</tt> parameter allows hard coding devices or using heuristics
+                    to prevent the user having to always select the best device to use.
+                    Whether guessing or not, a driver is still initialised.
+    @result         Returns a YES if initialised correctly, NO if unable to initialise the device,
+                    such as an unavailable sound interface.
+*/
 PERFORM_API BOOL SNDInit(BOOL guessTheDevice);
 
-// retrieve a list of available driver descriptions
+/*!
+    @function       SNDGetAvailableDriverNames
+    @abstract       Retrieve a list of available driver descriptions.
+    @result         Returns a NULL terminated array of readable strings of each driver's name.
+*/
 PERFORM_API char **SNDGetAvailableDriverNames(void);
 
-// assign currently active driver
+/*!
+    @function       SNDSetDriverIndex
+    @abstract       Assign currently active driver.
+    @param          selectedIndex
+                        The 0 base index into the driver table returned by SNDGetAvailableDriverNames
+    @result         Returns YES if able to set the index, no if unable (for example selectedIndex out of bounds).
+*/
 PERFORM_API BOOL SNDSetDriverIndex(unsigned int selectedIndex);
 
-// return the index into driverList currently selected.
+/*!
+    @function       SNDGetAssignedDriverIndex
+    @abstract       Return the index into driverList currently selected.
+    @result         Returns the index (0 base).
+*/
 PERFORM_API unsigned int SNDGetAssignedDriverIndex(void);
 
-PERFORM_API void SNDGetVolume(float *left, float * right);
+/*!
+    @function       SNDGetVolume
+    @abstract       Retrieve the current volume.
+    @param          left
+                        Receives the current left volume value (what units?).
+    @param          right
+                        Receives the current right volume value (what units?).
+*/
+PERFORM_API void SNDGetVolume(float *left, float *right);
 
+/*!
+    @function       SNDSetVolume
+    @abstract       Sets the current volume.
+    @param          left
+                        Sets the current left volume value (what units?).
+    @param          right
+                        Sets the current right volume value (what units?).
+    @result         Returns a readable string.
+*/
 PERFORM_API void SNDSetVolume(float left, float right);
 
+/*!
+    @function       SNDIsMuted
+    @abstract       Determine if the currently playing sound is muted.
+    @result         Returns YES if the currently playing sound is muted.
+*/
 PERFORM_API BOOL SNDIsMuted(void);
 
+/*!
+    @function       SNDSetMute
+    @abstract       Mute or unmute the currently playing sound..
+    @param          aFlag
+                        YES to mute, NO to unmute.
+*/
 PERFORM_API void SNDSetMute(BOOL aFlag);
 
+/*!
+    @function       SNDStartPlaying
+    @abstract       .
+    @discussion	    This function need not be implemented if sound streaming is supported.
+    @param          soundStruct
+                        .
+    @param          tag
+    @param          priority
+    @param          preempt
+    @param          beginFun
+    @param          endFun
+    @result         Returns .
+*/
 PERFORM_API int SNDStartPlaying(SndSoundStruct *soundStruct, int tag, int priority,  int preempt, 
   SNDNotificationFun beginFun, SNDNotificationFun endFun);
 
+/*!
+    @function       SNDStartRecording
+    @abstract       .
+    @discussion	    This function need not be implemented if sound streaming is supported.
+    @param          soundStruct
+                        .
+    @param          tag
+    @param          priority
+    @param          preempt
+    @param          beginRecFun
+    @param          endRecFun
+    @result         Returns .
+*/
 PERFORM_API int SNDStartRecording(SndSoundStruct *soundStruct, int tag, int priority, int preempt, 
   SNDNotificationFun beginRecFun, SNDNotificationFun endRecFun);
  
+/*!
+    @function       SNDSamplesProcessed
+    @abstract       .
+    @param          tag
+                        The integer tag indicating the sound to inspect.
+    @result         Returns the number of samples processed.
+*/
 PERFORM_API int SNDSamplesProcessed(int tag);
 
+/*!
+    @function       SNDStop
+    @abstract       .
+    @param          tag
+                        The integer tag indicating the sound to stop.
+*/
 PERFORM_API void SNDStop(int tag);
 
+/*!
+    @function       SNDPause
+    @abstract       .
+    @param          tag
+                        The integer tag indicating the sound to pause.
+*/
 PERFORM_API void SNDPause(int tag);
 
+/*!
+    @function       SNDResume
+    @abstract       .
+    @param          tag
+                        The integer tag indicating the sound to resume.
+*/
 PERFORM_API void SNDResume(int tag);
 
+/*!
+    @function       SNDUnreserve
+    @abstract       .
+    @param          dunno
+                        .
+    @result         Returns a .
+*/
 PERFORM_API int SNDUnreserve(int dunno);
 
+/*!
+    @function       SNDTerminate
+    @abstract       .
+*/
 PERFORM_API void SNDTerminate(void);
 
+/*!
+    @function       SNDStreamNativeFormat
+    @abstract       .
+    @param          streamFormat
+                        .
+*/
 PERFORM_API void SNDStreamNativeFormat(SndSoundStruct *streamFormat);
 
+/*!
+    @function       SNDStreamStart
+    @abstract       .
+    @param          newStreamProcessor
+                        .
+    @param          userData
+                        
+    @result         Returns YES if ?, NO if ?.
+*/
 PERFORM_API BOOL SNDStreamStart(SNDStreamProcessor newStreamProcessor, void *userData);
 
+/*!
+    @function       SNDStreamStop 
+    @abstract       .
+    @result         Returns YES if ?, NO if ?.
+*/
 PERFORM_API BOOL SNDStreamStop(void);
 
 #ifdef __cplusplus
