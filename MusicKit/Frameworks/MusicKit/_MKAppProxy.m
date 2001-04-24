82a531eafbdc49a657d3866f801a2e86bf11ca52
Leigh Smith <leigh@leighsmith.com>
2001-04-24 18:31:30 -0500

    Added copyright message

--- MusicKit/Frameworks/MusicKit/_MKAppProxy.m
+++ MusicKit/Frameworks/MusicKit/_MKAppProxy.m
@@ -1,3 +1,18 @@
+/*
+  $Id$
+  Defined In: The MusicKit
+
+  Description:
+    This class manages replies from the MusicKit thread back to the AppKit thread.
+    
+  Original Author: Leigh M. Smith
+
+  Copyright (c) 2001 tomandandy music inc.
+
+  Permission is granted to use and modify this code for commercial and non-commercial
+  purposes so long as the author attribution and this copyright message remains intact
+  and accompanies all derived code.
+*/
 /* AppProxy.m created by leigh on Thu 30-Mar-2000 */
 
 #import "_MKAppProxy.h"
