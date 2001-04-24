279d669266fad1d1b4c0d312c4e32ee3dc06e119
Leigh Smith <leigh@leighsmith.com>
2001-04-24 18:50:06 -0500

    Cleaned up for MacOSX

---
+++ MusicKit/Examples/metronome/Metronome.h
@@ -0,0 +1,15 @@
+#import <AppKit/AppKit.h>
+#import <MusicKit/MusicKit.h>
+//#import <MKSynthPatches/Pluck.h>
+
+@interface Metronome: NSObject {
+    id	tempoSlider;
+    MKNote *aNote;
+    Pluck *pluck;
+    MKConductor *cond;
+}
+
+- setTempoFromSlider:sender;
+- startStop:sender;
+
+@end
---
+++ MusicKit/Examples/metronome/Metronome.m
@@ -0,0 +1,45 @@
+#import "Metronome.h"
+
+@implementation Metronome
+
+- setTempoFromSlider:sender {
+    [MKConductor lockPerformance];
+    [cond setTempo:[sender doubleValue]];
+    [MKConductor unlockPerformance];
+    return self;
+}
+
+- appDidInit:sender  { /* Invoked when first message is sent to app */
+    aNote = [[MKNote alloc] init];          /* A note we'll use over and over */
+    [MKOrchestra setTimed:YES];             /* Use the DSP 'clock' */
+    [MKOrchestra new];                      /* Create Orchestra for all DSPs */
+    MKSetDeltaT(1.0);                     /* Scheduler advance   */
+    [MKConductor setFinishWhenEmpty:NO];    /* Keep going forever  */
+    cond = [MKConductor defaultConductor];
+    return self;
+}	
+
+- play {
+    [pluck noteOn:aNote];                  /* Do it */
+    [cond sel:@selector(play) to:self withDelay:1.0 argCount:0];
+    return self;
+}
+
+- startStop:sender {
+    if ([MKConductor inPerformance]) {
+	[MKConductor lockPerformance];
+	[MKConductor finishPerformance];
+	[MKConductor unlockPerformance];
+	[pluck dealloc];  /* Must dealloc by hand, 'cause we alloced by hand */
+	[MKOrchestra close];
+    } else {
+	[MKOrchestra open];
+	pluck = [MKOrchestra allocSynthPatch:[MKPluck class]];		
+	[cond sel:@selector(play) to:self withDelay:0 argCount:0];
+	[MKOrchestra run]; 
+	[MKConductor startPerformance];	    
+    }
+    return self;
+}
+
+@end
