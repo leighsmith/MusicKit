6e306436cd57d3c431920174b4ede81e24561094
Stephen Brandon <sbrandon@users.sourceforge.net>
2001-08-08 15:52:16 +0000

    lightweight data storage class for use in SndAudioFader

---
+++ MusicKit/Frameworks/SndKit/SKBreakpoint.h
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  SKBreakpoint.h
+//  SndKit
+//
+//  Created by Stephen Brandon on Wed Jun 25 2001. <stephen@brandonitconsulting.co.uk>
+//  Copyright (c) 2001 tomandandy music inc.
+//
+//  Permission is granted to use and modify this code for commercial and non-commercial
+//  purposes so long as the author attribution and copyright messages remain intact and
+//  accompany all relevant code.
+//
+////////////////////////////////////////////////////////////////////////////////
+#ifndef __SKBREAKPOINT_H__
+#define __SKBREAKPOINT_H__
+
+#import <Foundation/Foundation.h>
+
+@interface SKBreakpoint : NSObject
+{
+  int    flags;
+  float  yVal;
+  double xVal;
+}
+
+- initWithX:(double)x y:(float)y flags:(int)f;
+
+- (int)    getFlags;
+- (float)  getYVal;
+- (double) getXVal;
+
+- (void) setFlags: (int) f;
+- (void) setYVal: (float) yVal;
+- (void) setXVal: (double) xVal;
+
+/* allow easy array sorting */
+- (NSComparisonResult) compare:(SKBreakpoint *)other;
+
+@end
+
+#endif
+
