92ac1b11ac55775e33b4f1cda10fd84f5c9dcad1
Leigh Smith <leigh@leighsmith.com>
2001-09-08 16:22:05 -0500

    Added doco that mysteriously was removed

--- MusicKit/Frameworks/MKSynthPatches/ArielQPMix.h
+++ MusicKit/Frameworks/MKSynthPatches/ArielQPMix.h
@@ -14,6 +14,9 @@
 */
 /*
   $Log$
+  Revision 1.3  2001/09/08 21:22:05  leighsmith
+  Added doco that mysteriously was removed
+
   Revision 1.2  2001/09/08 20:22:09  leighsmith
   Merged RTF Reference documentation into headerdoc comments and prepended MK to any older class names
 
@@ -25,7 +28,13 @@
 <b>ArielQPMix</b> is a simple SynthPatch designed to be used with the Ariel
 QuintProcessor.  Instantiating an instance of <b>ArielQPMix</b> causes the sound
 output from the four satellite DSPs to be summed into the output of the hub DSP.
-<b>ArielQPMix</b> has no parameters, no methods and no 
+<b>ArielQPMix</b> has no parameters, no methods and no instance variables.
+
+Keep in mind that <b>ArielQPMix</b> need not be
+used.  Instead, you may handle the sound streams from the four satellites in
+some other way.   For example, see the programming example
+<b>QP/QuintClusters</b>, which processes the satellites' output in a different
+way.
 */
 #ifndef __MK_ArielQPMix_H___
 #define __MK_ArielQPMix_H___
