24c757df11f1e37f424c33b38acc02ece6ad1466
Stephen Brandon <sbrandon@users.sourceforge.net>
2001-10-03 10:46:52 +0000

    added comments regarding how to obtain the lame and icecast libraries, and a warning about the fact they are GPL.

--- MusicKit/Frameworks/SndKit/SndAudioProcessorMP3Encoder.h
+++ MusicKit/Frameworks/SndKit/SndAudioProcessorMP3Encoder.h
@@ -3,12 +3,28 @@
 //  SndKit
 //
 //  Created by skot on Mon Oct 01 2001.
-//  Copyright (c) 2001 __CompanyName__. All rights reserved.
+//  Copyright (c) 2001 tomandandy music inc All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import "SndAudioProcessor.h"
 #import "SndAudioBuffer.h"
+
+// "lame" can be obtained via http://www.mp3dev.org and must be installed before
+// the SndKit will compile.
+//
+// The <shout/shout.h> header is NOT part of the shoutcast server, but part of
+// the icecast project. Download and install the "iceS" package
+// from http://www.icecast.org before compiling the SndKit. If you also want to
+// run the icecast server from the same machine, also download the "icecast"
+// package from the same location, and install.
+// The "iceS" package provides the <shout/shout.h> header, and is the part of
+// the mechanism for sending data to the icecast server itself.
+
+// the iceS library is under the GPL license, so before using the SndKit with
+// this facility enabled, please ensure that you understand the implications
+// of this.
+
 #import <lame/lame.h>
 #import <shout/shout.h>
 
