e1416017a5ff588c48a55266048272d49841492f
SKoT McDonald <skot@users.sourceforge.net>
2001-11-01 13:35:52 -0500

    Added URLs to license statements by LAME and icecast dev crews... so we can keep an eye on em... (mistrust, mistrust)

--- MusicKit/Frameworks/SndKit/SndAudioProcessorMP3Encoder.h
+++ MusicKit/Frameworks/SndKit/SndAudioProcessorMP3Encoder.h
@@ -9,23 +9,25 @@
 #import "SndAudioProcessor.h"
 #import "SndAudioBuffer.h"
 
+#import <lame/lame.h>
 // "lame" can be obtained via http://www.mp3dev.org and must be installed before
-// the SndKit will compile.
-//
+// the SndKit will compile. LAME encoder is LGPL
+// License statement: http://www.sulaco.org/mp3/license.txt
+
+#import <shout/shout.h>
 // The <shout/shout.h> header is NOT part of the shoutcast server, but part of
-// the icecast project. Download and install the "iceS" package
+// the icecast project. Download and install the "libshout" package
 // from http://www.icecast.org before compiling the SndKit. If you also want to
 // run the icecast server from the same machine, also download the "icecast"
 // package from the same location, and install.
-// The "iceS" package provides the <shout/shout.h> header, and is the part of
+// The "libshout" package provides the <shout/shout.h> header, and is the part of
 // the mechanism for sending data to the icecast server itself.
 
-// the iceS library is under the GPL license, so before using the SndKit with
-// this facility enabled, please ensure that you understand the implications
-// of this.
+// The icecast libshout library is under the Lesser GPL license, so before 
+// using the SndKit with this facility enabled, please ensure that you 
+// understand the implications of this. The license statement can be found
+// in the README file in the libshout source directory.
 
-#import <lame/lame.h>
-#import <shout/shout.h>
 
 @interface SndAudioProcessorMP3Encoder : SndAudioProcessor {
   float             *buffer_l;
