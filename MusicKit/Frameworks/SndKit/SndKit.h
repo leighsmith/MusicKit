90c728d3d026a6590d915bf356160d516f180f60
Leigh Smith <leigh@leighsmith.com>
2003-06-18 14:02:01 -0500

    Added SndFormat.h

--- MusicKit/Frameworks/SndKit/SndKit.h
+++ MusicKit/Frameworks/SndKit/SndKit.h
@@ -1,32 +1,43 @@
 /******************************************************************************
-LEGAL:
-This framework and all source code supplied with it, except where specified, are
-Copyright Stephen Brandon and the University of Glasgow, 1999. You are free to
-use the source code for any purpose, including commercial applications, as long
-as you reproduce this notice on all such software.
+  $Id$
 
-Software production is complex and we cannot warrant that the Software will be
-error free.  Further, we will not be liable to you if the Software is not fit
-for the purpose for which you acquired it, or of satisfactory quality. 
+  Description: Main class defining a sound object.
 
-WE SPECIFICALLY EXCLUDE TO THE FULLEST EXTENT PERMITTED BY THE COURTS ALL
-WARRANTIES IMPLIED BY LAW INCLUDING (BUT NOT LIMITED TO) IMPLIED WARRANTIES OF
-QUALITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT OF THIRD PARTIES
-RIGHTS.
+  Original Author: Stephen Brandon
 
-If a court finds that we are liable for death or personal injury caused by our
-negligence our liability shall be unlimited.  
+  Copyright (c) 1999 Stephen Brandon and the University of Glasgow
+  Additions Copyright (c) 2001, The MusicKit Project.  All rights reserved.
 
-WE SHALL HAVE NO LIABILITY TO YOU FOR LOSS OF PROFITS, LOSS OF CONTRACTS, LOSS
-OF DATA, LOSS OF GOODWILL, OR WORK STOPPAGE, WHICH MAY ARISE FROM YOUR
-POSSESSION OR USE OF THE SOFTWARE OR ASSOCIATED DOCUMENTATION.  WE SHALL HAVE
-NO LIABILITY IN RESPECT OF ANY USE OF THE SOFTWARE OR THE ASSOCIATED
-DOCUMENTATION WHERE SUCH USE IS NOT IN COMPLIANCE WITH THE TERMS AND CONDITIONS
-OF THIS AGREEMENT.
+  Legal Statement:
+
+    This framework and all source code supplied with it, except where specified, are
+    Copyright Stephen Brandon and the University of Glasgow, 1999. You are free to
+    use the source code for any purpose, including commercial applications, as long
+    as you reproduce this notice on all such software.
+
+    Software production is complex and we cannot warrant that the Software will be
+    error free.  Further, we will not be liable to you if the Software is not fit
+    for the purpose for which you acquired it, or of satisfactory quality. 
+
+    WE SPECIFICALLY EXCLUDE TO THE FULLEST EXTENT PERMITTED BY THE COURTS ALL
+    WARRANTIES IMPLIED BY LAW INCLUDING (BUT NOT LIMITED TO) IMPLIED WARRANTIES OF
+    QUALITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT OF THIRD PARTIES
+    RIGHTS.
+    
+    If a court finds that we are liable for death or personal injury caused by our
+    negligence our liability shall be unlimited.  
+    
+    WE SHALL HAVE NO LIABILITY TO YOU FOR LOSS OF PROFITS, LOSS OF CONTRACTS, LOSS
+    OF DATA, LOSS OF GOODWILL, OR WORK STOPPAGE, WHICH MAY ARISE FROM YOUR
+    POSSESSION OR USE OF THE SOFTWARE OR ASSOCIATED DOCUMENTATION.  WE SHALL HAVE
+    NO LIABILITY IN RESPECT OF ANY USE OF THE SOFTWARE OR THE ASSOCIATED
+    DOCUMENTATION WHERE SUCH USE IS NOT IN COMPLIANCE WITH THE TERMS AND CONDITIONS
+    OF THIS AGREEMENT.
 
 ******************************************************************************/
 
 #import <MKPerformSndMIDI/PerformSound.h>
+#import "SndFormat.h"
 #import "SndEndianFunctions.h"
 #import "SndTable.h"
 #import "Snd.h"
