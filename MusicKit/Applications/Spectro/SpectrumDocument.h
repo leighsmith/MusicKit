b0de80c7d325f7de6300aa3de2bc9088c8100f97
Leigh Smith <leigh@leighsmith.com>
2003-08-20 00:10:17 -0500

    Removed SubSoundView.h import, added IBOutlet declarations to implicit outlets

--- MusicKit/Applications/Spectro/SpectrumDocument.h
+++ MusicKit/Applications/Spectro/SpectrumDocument.h
@@ -1,4 +1,6 @@
-/* SpectrumDocument.h -- Interface for SpectrumDocument class
+/* 
+ * $Id$
+ * Interface for SpectrumDocument class
  *
  * This object manages the computation and display of Spectrum data
  * via the SpectrumWindow, SpectrumView, ScrollingSpectrum, and the
@@ -6,68 +8,76 @@
  
  * An instance of this object is created for each separate sound.
  *
- * Created by Gary Scavone.  Last modified: 4/94.
+ * Created by Gary Scavone.
+ *
+ * Modifications Copyright (c) 2003 The MusicKit Project, All Rights Reserved.
+ *
+ * Legal Statement Covering Additions by The MusicKit Project:
+ *
+ *   Permission is granted to use and modify this code for commercial and
+ *   non-commercial purposes so long as the author attribution and copyright
+ *   messages remain intact and accompany all relevant code.
  *
  */
 
-#import <Foundation/NSObject.h>
-#import "SubSoundView.h"
+#import <Foundation/Foundation.h>
+#import <SndKit/SndKit.h>
 #import "SpectrumView.h"
 
-@interface SpectrumDocument:NSObject
+@interface SpectrumDocument: NSObject
 {
-    id spectrumWindow;
-    id scrollSpectrum;
-	id waterfallWindow;
-	id myWaterfallView;
-	id mySignalProcessor;
-	id mySound;
-	id delegate;
-    id waterFallButton;
-	id cursorFreq;
-	id cursorAmp;
-	id sineError;
-	id peakFreqCell;
-	id windowSizeCell;
-	id freqRangeMatrix;
-	id wfFreqRangeMatrix;
-	id wfTimeRangeMatrix;
-	id wfPlotHeight;
-	id dBLimitsMatrix;
-	id zpFactorCell;
-	id hopRatioCell;
-	id windowTypeButton;
-	id frameSlider;
-	id wfFrameSlider;
-	id currentFrameCell;
-	id wfCurrentFrameCell;
-	id wfCurrentFrameTime;
-	id totalFramesCell;
-	int dataSize;
-	int windowSize;
-	int firstSample;
-	int sampleCount;
-	int totalFrames;
-	int currentFrame;
-	int channel;
-	int power;
-	int lastLength;
-	int lobeWidth;
-	float hopRatio;
-	float displayPoints;
-	float zpFactor;
-	float *samples;
-	float *window;
-	float windowArea;
-	float ceiling;
-	NSString *windowType;
-	SubSoundView *mySoundView;
+    IBOutlet id spectrumWindow;
+    IBOutlet id scrollSpectrum;
+    IBOutlet id waterfallWindow;
+    IBOutlet id myWaterfallView;
+    IBOutlet id mySignalProcessor;
+    IBOutlet id mySound;
+    IBOutlet id delegate;
+    IBOutlet id waterFallButton;
+    IBOutlet id cursorFreq;
+    IBOutlet id cursorAmp;
+    IBOutlet id sineError;
+    IBOutlet id peakFreqCell;
+    IBOutlet id windowSizeCell;
+    IBOutlet id freqRangeMatrix;
+    IBOutlet id wfFreqRangeMatrix;
+    IBOutlet id wfTimeRangeMatrix;
+    IBOutlet id wfPlotHeight;
+    IBOutlet id dBLimitsMatrix;
+    IBOutlet id zpFactorCell;
+    IBOutlet id hopRatioCell;
+    IBOutlet id windowTypeButton;
+    IBOutlet id frameSlider;
+    IBOutlet id wfFrameSlider;
+    IBOutlet id currentFrameCell;
+    IBOutlet id wfCurrentFrameCell;
+    IBOutlet id wfCurrentFrameTime;
+    IBOutlet id totalFramesCell;
+    int dataSize;
+    int windowSize;
+    int firstSample;
+    int sampleCount;
+    int totalFrames;
+    int currentFrame;
+    int channel;
+    int power;
+    int lastLength;
+    int lobeWidth;
+    float hopRatio;
+    float displayPoints;
+    float zpFactor;
+    float *samples;
+    float *window;
+    float windowArea;
+    float ceiling;
+    NSString *windowType;
+    SndView *mySoundView;
     SpectrumView *mySpectrumView;
-	BOOL sliderCall;
-	BOOL miniature;
-	BOOL newWindow;
-	BOOL stop;
-	BOOL wfSliderEnabled;
+    BOOL sliderCall;
+    BOOL miniature;
+    BOOL newWindow;
+    BOOL stop;
+    BOOL wfSliderEnabled;
 }
 
 - init;
