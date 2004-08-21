295f0a475e951f5ba83f2ed565f5a48de366cb1c
Leigh Smith <leigh@leighsmith.com>
2004-08-21 17:55:11 -0500

    Added subclass of NSScroller which allows resizing by dragging the ends of the scroller

---
+++ MusicKit/Frameworks/SndKit/SndStretchableScroller.h
@@ -0,0 +1,40 @@
+//
+//  $Id$
+//
+//  Original Author: Leigh Smith and Skot McDonald.
+//
+//  Copyright (c) 2004, The MusicKit Project.  All rights reserved.
+//
+//  Permission is granted to use and modify this code for commercial and 
+//  non-commercial purposes so long as the author attribution and copyright 
+//  messages remain intact and accompany all relevant code.
+//
+
+#import <AppKit/AppKit.h>
+
+/*!
+  @class SndStretchableScroller
+  @abstract A subclass of NSScroller that adds two handles that allows stretching the scroller, signalling that
+            the enclosed scrollable object should be resized, typically changing magnification.
+ */
+@interface SndStretchableScroller: NSScroller {
+  /*! stretchingLeftKnob YES when the left knob is being stretched. */
+  BOOL stretchingLeftKnob;
+  /*! stretchingRightKnob YES when the right knob is being stretched. */
+  BOOL stretchingRightKnob;
+}
+
+/*!
+  @method drawKnob
+  @abstract The new drawing method which draws the stretch icons.
+ */
+- (void) drawKnob;
+
+/*!
+  @method mouseDown:
+  @abstract The new mouse event method that detects and updates the scroller.
+  @param theEvent An NSEvent
+ */ 
+- (void) mouseDown: (NSEvent *) theEvent;
+
+@end
