ef63424d7e5e9f657b907f9f097939a1efe9ef82
Leigh Smith <leigh@leighsmith.com>
2004-10-26 12:29:42 -0500

    Typed parameters

--- MusicKit/Frameworks/MusicKit/MKNoteFilter.h
+++ MusicKit/Frameworks/MusicKit/MKNoteFilter.h
@@ -3,34 +3,14 @@
   Defined In: The MusicKit
 
   Description:
-    MKNoteFilter is an abstract class that combines the functionality it
-    inherits from MKInstrument with the protocol defined in the MKPerformer
-    class.  MKNoteFilter objects can both receive and send MKNotes; they're
-    interposed between MKPerformers and MKInstruments to create a MKNote
-    processing pipeline.  The subclass responsibility
-    realizeNote:fromNoteReceiver: is passed on to MKNoteFilter subclasses.
-    Keep in mind that notes must be copied on write or store.
+    See class description below for description.    
 
   Original Author: David A. Jaffe
 
   Copyright (c) 1988-1992, NeXT Computer, Inc.
   Portions Copyright (c) 1994 NeXT Computer, Inc. and reproduced under license from NeXT
   Portions Copyright (c) 1994 Stanford University
-*/
-/*
-  $Log$
-  Revision 1.5  2001/09/06 21:27:47  leighsmith
-  Merged RTF Reference documentation into headerdoc comments and prepended MK to any older class names
-
-  Revision 1.4  2000/04/25 02:11:02  leigh
-  Renamed free methods to release methods to reflect OpenStep behaviour
-
-  Revision 1.3  2000/04/02 17:12:08  leigh
-  Cleaned up doco
-
-  Revision 1.2  1999/07/29 01:25:46  leigh
-  Added Win32 compatibility, CVS logs, SBs changes
-
+  Portions Copyright (c) 1999-2004, The MusicKit Project.
 */
 /*!
   @class MKNoteFilter
@@ -38,13 +18,12 @@
             from MKInstrument with the protocol defined in the MKPerformer class. 
 
   @discussion
-  
-MKNoteFilter is an abstract class that combines the functionality it inherits
-from MKInstrument with the protocol defined in the MKPerformer class. 
-MKNoteFilter objects can both receive and send MKNotes; they're interposed
-between MKPerformers and MKInstruments to create a MKNote processing pipeline. 
-The subclass responsibility <b>realizeNote:fromNoteReceiver:</b> is passed on to
-MKNoteFilter subclasses.
+    MKNoteFilter is an abstract class that combines the functionality it inherits
+    from MKInstrument with the protocol defined in the MKPerformer class. 
+    MKNoteFilter objects can both receive and send MKNotes; they're interposed
+    between MKPerformers and MKInstruments to create a MKNote processing pipeline. 
+    The subclass responsibility <b>realizeNote:fromNoteReceiver:</b> is passed on to
+    MKNoteFilter subclasses. Keep in mind that notes must be copied on write or store.
 */
 #ifndef __MK_NoteFilter_H___
 #define __MK_NoteFilter_H___
@@ -65,19 +44,19 @@ MKNoteFilter subclasses.
 
 /*!
   @method noteSenders
-  @result Returns an id.
+  @result Returns an NSArray instance.
   @discussion Returns a copy of the receiver's NSArray of MKNoteSenders.
 */
-- noteSenders;
+- (NSArray *) noteSenders;
 
 /*!
   @method isNoteSenderPresent:
-  @param  aNoteSender is an id.
+  @param  aNoteSender is an MKNoteSender instance.
   @result Returns a BOOL.
   @discussion Returns YES if <i>aNoteSender</i> is one of the receiver's
               MKNoteSenders.  Otherwise returns NO.
 */
--(BOOL ) isNoteSenderPresent:aNoteSender; 
+- (BOOL) isNoteSenderPresent: (MKNoteSender *) aNoteSender; 
 
 /*!
   @method copyWithZone:
@@ -87,72 +66,64 @@ MKNoteFilter subclasses.
               new object contains copies of the receiver's MKNoteSenders and
               MKNoteReceivers.
 */
-- copyWithZone:(NSZone *)zone;
+- copyWithZone: (NSZone *) zone;
 
 /*!
   @method releaseNoteSenders
   @result Returns an id.
-  @discussion Removes and frees the receiver's MKNoteSenders. Returns the
-              receiver.
+  @discussion Removes and frees the receiver's MKNoteSenders. Returns the receiver.
 */
 - releaseNoteSenders;
 
 /*!
   @method removeNoteSenders
   @result Returns an id.
-  @discussion Removes all the receiver's MKNoteSenders.  Returns the
-              receiver.
+  @discussion Removes all the receiver's MKNoteSenders. Returns the receiver.
 */
 - removeNoteSenders;
 
 /*!
   @method noteSender
-  @result Returns an id.
+  @result Returns an MKNoteSender instance.
   @discussion Returns the receiver's first MKNoteSender.  This is method should
               only by invoked if the receiver only contains one MKNoteSender or if
               you don't care which MKNoteSender you get.  If there are currently
-              no MKNoteSenders, this method creates and adds a
-              MKNoteSender.
+              no MKNoteSenders, this method creates and adds a MKNoteSender.
 */
-- noteSender;
+- (MKNoteSender *) noteSender;
 
 /*!
   @method addNoteSender:
-  @param  aNoteSender is an id.
+  @param  aNoteSender is an MKNoteSender instance.
   @result Returns an id.
   @discussion Removes <i>aNoteSender</i> from its present owner (if any) and adds
               it to the receiver.  Returns <i>aNoteSender</i>.  If the receiver is
               in performance, or if <i>aNoteSender</i> is already a member of the
               receiver, does nothing and returns <b>nil</b>.
 */
-- addNoteSender:aNoteSender; 
+- addNoteSender: (MKNoteSender *) aNoteSender; 
 
 /*!
   @method removeNoteSender:
-  @param  aNoteSender is an id.
+  @param  aNoteSender is an MKNoteSender instance.
   @result Returns an id.
   @discussion Removes aNoteSender from the receiver's NSArray of MKNoteSenders. 
               Returns <i>aNoteSender</i>.  If the receiver is in a performance,
               does nothing and returns <b>nil</b>.
 */
-- removeNoteSender:aNoteSender; 
+- removeNoteSender: (MKNoteSender *) aNoteSender; 
 
- /*
-  * Sends releaseNoteSenders and releaseNoteReceivers to the receiver
-  * then frees the receiver.
-  */
-- (void)dealloc; 
+/*!
+  @method dealloc
+  @discussion Sends releaseNoteSenders to the receiver then releases the receivers ivars.
+ */
+- (void) dealloc; 
 
-   /* 
-     You never send this message directly.  
-     Should be invoked with NXWriteRootObject(). 
-     Invokes superclass write: then archives noteSener List. */
-- (void)encodeWithCoder:(NSCoder *)aCoder;
+/* 
+  You never send this message directly. Archives noteSender Array. 
+ */
+- (void) encodeWithCoder: (NSCoder *) aCoder;
 
-   /* 
-     You never send this message directly.  
-     Should be invoked via NXReadObject(). 
-     See write:. */
 - (id)initWithCoder:(NSCoder *)aDecoder;
 
 @end
--- MusicKit/Frameworks/MusicKit/noteDispatcherMethods.m
+++ MusicKit/Frameworks/MusicKit/noteDispatcherMethods.m
@@ -3,30 +3,17 @@
   Defined In: The MusicKit
 
   Description:
+    Methods used in MKPerformer and MKNoteFilter.
+ 
   Original Author: David Jaffe
 
   Copyright (c) 1988-1992, NeXT Computer, Inc.
   Portions Copyright (c) 1994 NeXT Computer, Inc. and reproduced under license from NeXT
-  Portions Copyright (c) 1994 Stanford University
+  Portions Copyright (c) 1994 Stanford University.
+  Portions Copyright (c) 1999-2004, The MusicKit Project.
 */
 /*
-Modification history:
-
-  $Log$
-  Revision 1.6  2001/09/06 21:27:48  leighsmith
-  Merged RTF Reference documentation into headerdoc comments and prepended MK to any older class names
-
-  Revision 1.5  2000/04/25 02:08:39  leigh
-  Renamed free methods to release methods to reflect OpenStep behaviour
-
-  Revision 1.4  2000/04/16 04:05:07  leigh
-  comment cleanup
-
-  Revision 1.3  2000/04/04 00:14:10  leigh
-  Removed crufty include file
-
-  Revision 1.2  1999/07/29 01:26:10  leigh
-  Added Win32 compatibility, CVS logs, SBs changes
+ Modification history prior to commit to CVS:
 
   09/26/90/daj & lbj - Changed freeNoteSenders to make sure receiver's not
                        in performance. 
@@ -35,9 +22,9 @@ Modification history:
   03/08/95/daj - Fixed bug in removeNoteSenders.
 */
 
--noteSenders
+- (NSArray *) noteSenders
   /* TYPE: Processing
-   * Returns a copy of the receiver's NoteSender Array. 
+   * Returns a copy of the receiver's MKNoteSender Array. 
    */
 {
      return _MKLightweightArrayCopy(noteSenders);
@@ -47,14 +34,14 @@ Modification history:
 //   return [noteSenders copy];  // This copies a new array but the elements are the original noteSenders.
                                // If we copied the noteSenders we couldn't then connect senders 
 
--(BOOL)isNoteSenderPresent:(id)aNoteSender
+- (BOOL) isNoteSenderPresent: (MKNoteSender *) aNoteSender
   /* Returns YES if aNoteSender is a member of the receiver's 
-     NoteSender List. */
+     MKNoteSender Array. */
 {
-    return [noteSenders containsObject:aNoteSender];
+    return [noteSenders containsObject: aNoteSender];
 }
 
--releaseNoteSenders
+- releaseNoteSenders
   /* TYPE: Creating
    * Empties and frees contents of noteSenders.
    * Returns the receiver.
@@ -62,7 +49,7 @@ Modification history:
 {
     NSMutableArray * aList; //sb: static type
     if ([self inPerformance])
-      return nil;
+	return nil;
     aList = _MKLightweightArrayCopy(noteSenders);
     [self removeNoteSenders];
     [aList removeAllObjects];  /* Split this up because elements may try
@@ -72,33 +59,33 @@ Modification history:
     return self;
 }
 
--removeNoteSenders
+- removeNoteSenders
   /* Empties noteSenders by repeatedly sending removeNoteSender:
      with each element of the collection as the argument. */
 {
     unsigned i;
+    
     if (!noteSenders)
-      return self;
+	return self;
     i = [noteSenders count]; 
     /* We remove them in the reverse order, which should work (!) */
     while (i--) 
-      [self removeNoteSender:[noteSenders objectAtIndex:i]];
+	[self removeNoteSender: [noteSenders objectAtIndex: i]];
     return self;
 }
 
--noteSender
-  /* Returns the default NoteSender. This is used when you don't care
-     which NoteSender you get. */
+- (MKNoteSender *) noteSender
+  /* Returns the default MKNoteSender. This is used when you don't care
+     which MKNoteSender you get. */
 {
     if ([noteSenders count] == 0)
-        [self addNoteSender:[[MKNoteSender alloc] init]];
-    return [noteSenders objectAtIndex:0];
+        [self addNoteSender: [[MKNoteSender alloc] init]];
+    return [noteSenders objectAtIndex: 0];
 }
 
--disconnectNoteSenders
+- disconnectNoteSenders
     /* Broadcasts "disconnect" to MKNoteSenders. */ 
 {
-    [noteSenders makeObjectsPerformSelector:@selector(disconnect)];
+    [noteSenders makeObjectsPerformSelector: @selector(disconnect)];
     return self;
 }
-
