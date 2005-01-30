218c04e2f63df4835b95da7971b4d1a79ee91692
Leigh Smith <leigh@leighsmith.com>
2005-01-30 18:59:42 -0500

    Corrected slider drawing

--- MusicKit/Applications/Spectro/WaterfallView.m
+++ MusicKit/Applications/Spectro/WaterfallView.m
@@ -1,47 +1,51 @@
-/* WaterfallView.m */
+/* 
+ * $Id$ 
+ *
+ * Modifications Copyright (c) 2005 The MusicKit Project, All Rights Reserved.
+ *
+ * Legal Statement Covering Additions by The MusicKit Project:
+ *
+ *   Permission is granted to use and modify this code for commercial and
+ *   non-commercial purposes so long as the author attribution and copyright
+ *   messages remain intact and accompany all relevant code.
+ *
+ */
 
 #import "WaterfallView.h"
 #import "SpectrumDocument.h"
 #import "SoundController.h"
 #import <AppKit/AppKit.h>
-#import <AppKit/NSColor.h>
-
-#import <string.h>
+//#import <AppKit/NSColor.h>
 
 #define WIDTH     [self bounds].size.width		/* Width of view in pixels */
 #define HEIGHT    [self bounds].size.height		/* Height of view in pixels */
 
 @implementation WaterfallView
 
-- initWithFrame:(NSRect)theFrame
+- initWithFrame: (NSRect) theFrame
 {
     NSRect tempRect = theFrame;
     
-    [super initWithFrame:tempRect];
-/*
-    bbox[0] = 0;
-    bbox[1] = 0;
-    bbox[2] = [self bounds].origin.x + WIDTH + 1;
-    bbox[3] = [self bounds].origin.y + HEIGHT + 1;
-*/
+    [super initWithFrame: tempRect];
     lastLength = 0;
-    lastFrame = 0;
     currentFrame = 0;
-    draw = NO;
-    placeSliderLine = NO;
     cursorPixel = 0;
-
+    
     [self setColors];
     
     return self;
 }
-- (oneway void)dealloc
+
+- (oneway void) dealloc
 {
     [spectrumColor release];
     [cursorColor release];
+    [waterfallColor release];
+    // TODO we're leaking like a sieve here...
+    // waterFallPaths
 }
 
-- (void)setDelegate:(id)anObject
+- (void) setDelegate: (id) anObject
 {
     delegate = anObject;
 }
@@ -51,27 +55,26 @@
     return delegate;
 }
 
-- setup:(int)numFrames length:(int)numPoints
+- setup: (int) numFrames length: (int) numPoints
 {
     int frameIndex;
-
+    
     deltaX = [self bounds].size.width * 0.2 / (numFrames + 2);
     deltaY = HEIGHT / (numFrames + 2);
     length = numPoints;
     yNorm = [frameHeight floatValue] / (numFrames + 2);
     totalFrames = numFrames;
     currentFrame = numFrames - 1;
-
-    if (waterFallPath)
-        free(waterFallPath);
-    waterFallPath = (NSBezierPath **) malloc(numFrames * sizeof(NSBezierPath *));
+    
+    if (waterFallPaths)
+        free(waterFallPaths);
+    waterFallPaths = (NSBezierPath **) malloc(numFrames * sizeof(NSBezierPath *));
     for(frameIndex = 0; frameIndex < numFrames; frameIndex++) {
-        waterFallPath[frameIndex] = [[NSBezierPath bezierPath] retain];
+        waterFallPaths[frameIndex] = [[NSBezierPath bezierPath] retain];
     }
     if (length != lastLength) {
         lastLength = length;
     }
-    draw = YES;
     return self;
 }
 
@@ -86,11 +89,11 @@
     xmax = WIDTH;
     xoffs = currentFrame * deltaX;
     yoffs = currentFrame * deltaY + 2.0;
-
+    
     frameIndex = (totalFrames - currentFrame - 1);
     waterFallPoint.x = xoffs;
     waterFallPoint.y = yoffs;
-    [waterFallPath[frameIndex] moveToPoint: waterFallPoint];
+    [waterFallPaths[frameIndex] moveToPoint: waterFallPoint];
     for (i = 0; i < length; i++) {
         waterFallPoint.x = xoffs + xstep * i;		/* X coord */
         if (coefs[i] > max) max = coefs[i];
@@ -98,81 +101,88 @@
         if (temp > HEIGHT) temp = HEIGHT;
         waterFallPoint.y = temp;			/* Y coord */
         max = 0.0;
-        [waterFallPath[frameIndex] lineToPoint: waterFallPoint];
+        [waterFallPaths[frameIndex] lineToPoint: waterFallPoint];
     }
     waterFallPoint.x = xoffs + xstep * i;
     waterFallPoint.y = yoffs;
-    [waterFallPath[frameIndex] lineToPoint: waterFallPoint];
-
+    [waterFallPaths[frameIndex] lineToPoint: waterFallPoint];
+    
     waterFallPoint.x = deltaX + xoffs + xstep * i;
     waterFallPoint.y = yoffs;
-    [waterFallPath[frameIndex] lineToPoint: waterFallPoint];
-
+    [waterFallPaths[frameIndex] lineToPoint: waterFallPoint];
+    
     currentFrame--;
     return self;
 }
 
--(BOOL)isOpaque
+- (BOOL) isOpaque
 {
     return YES;
 }
 
-- (void)drawRect:(NSRect)rects
+// Returns the region of the display drawn using a slider at the given spectral frame.
+- (NSRect) rectOfSliderAtFrame: (int) frame
 {
-    NSBezierPath *sliderPath = [NSBezierPath bezierPath];		/* path for slider line */
+    NSRect sliderRectangle;
+    
+    sliderRectangle.origin.x = WIDTH - (totalFrames - frame) * deltaX;
+    sliderRectangle.origin.y = deltaY * frame + 1.0;
+    sliderRectangle.size.width = (totalFrames - frame) * deltaX;
+    sliderRectangle.size.height = 2.0;
 
-    if (!placeSliderLine)
-        NSEraseRect([self bounds]);
-    if (!draw)
-        return;
+    return sliderRectangle;
+}
 
-    if (!placeSliderLine) {
-        int frameIndex;
-        for (frameIndex = 0; frameIndex < totalFrames; frameIndex++) {
-            [waterfallColor set]; 
-            [waterFallPath[frameIndex] fill];
-
-            [spectrumColor set];
-            [waterFallPath[frameIndex] stroke];
-        }
+- (void) drawRect: (NSRect) updateRect
+{
+    NSRect currentFrameRect;
+    int frameIndex;
+    
+    NSEraseRect(updateRect);
+	
+    for (frameIndex = 0; frameIndex < totalFrames; frameIndex++) {
+	// Since drawing the waterfall can be slow, we check which regions we need to redraw.
+	if(NSIntersectsRect(updateRect, [waterFallPaths[frameIndex] bounds])) {
+	    // NSLog(@"drawing waterfall frame %d\n", frameIndex);
+	    // NSLog(@"updateRect %f, %f, %f, %f\n", updateRect.origin.x, updateRect.origin.y, updateRect.size.width, updateRect.size.height);
+	    // NSLog(@"waterFallPaths bounds %f, %f, %f, %f\n", [waterFallPaths[frameIndex] bounds].origin.x, [waterFallPaths[frameIndex] bounds].origin.y, [waterFallPaths[frameIndex] bounds].size.width, [waterFallPaths[frameIndex] bounds].size.height);
+	    [waterfallColor set]; 
+	    [waterFallPaths[frameIndex] fill];
+	    
+	    [spectrumColor set];
+	    [waterFallPaths[frameIndex] stroke];
+	}
     }
-
-    if (placeSliderLine) {
+    
+    currentFrameRect = [self rectOfSliderAtFrame: currentFrame];
+    
+    if (NSIntersectsRect(updateRect, currentFrameRect)) {
+	NSBezierPath *sliderPath = [NSBezierPath bezierPath];		/* path for slider line */
         NSPoint sliderMovePoint, sliderDrawPoint;
-        sliderMovePoint.x = WIDTH;
-        sliderMovePoint.y = deltaY * lastFrame + 2.0;
-        sliderDrawPoint.x = WIDTH - (totalFrames - lastFrame) * deltaX;
-        sliderDrawPoint.y = deltaY * lastFrame + 2.0;
-        // PSsetgray(1.0);
-        [[NSColor blackColor] set];
-        [sliderPath moveToPoint: sliderMovePoint];
-        [sliderPath lineToPoint: sliderDrawPoint];
-        [sliderPath stroke];
 
-        sliderMovePoint.x = WIDTH;
-        sliderMovePoint.y = deltaY * currentFrame + 2.0;
-        sliderDrawPoint.x = WIDTH - (totalFrames - currentFrame) * deltaX;
-        sliderDrawPoint.y = deltaY * currentFrame + 2.0;
+	// NSLog(@"currentFrameRect %f, %f, %f, %f\n", currentFrameRect.origin.x, currentFrameRect.origin.y, currentFrameRect.size.width, currentFrameRect.size.height);
+
+	sliderMovePoint.y = sliderDrawPoint.y = currentFrameRect.origin.y + 1.0;
+	sliderMovePoint.x = currentFrameRect.origin.x;
+        sliderDrawPoint.x = currentFrameRect.origin.x + currentFrameRect.size.width;
 
         [cursorColor set];
         [sliderPath moveToPoint: sliderMovePoint];
         [sliderPath lineToPoint: sliderDrawPoint];
         [sliderPath stroke];
-        [self setNeedsDisplay: YES]; /* because we used up the 1st one on printing the slider lines */
     }
 }
 
-- placeSliderLineAt:(int)frameNum
+- (void) placeSliderLineAt: (int) frameNum
 {
-	currentFrame = frameNum;
-	placeSliderLine = YES;
-        [self display];
-	placeSliderLine = NO;
-	lastFrame = frameNum;
-	return self;
+    // Mark the previous frame rectangle for redraw, thus erasing it.
+    [self setNeedsDisplayInRect: [self rectOfSliderAtFrame: currentFrame]];
+    currentFrame = frameNum;
+    // Mark the current frame rectangle for redraw, drawing the slider
+    [self setNeedsDisplayInRect: [self rectOfSliderAtFrame: currentFrame]];
 }
 
-- setColors
+- (void) setColors
 {
     [spectrumColor release];
     [waterfallColor release];
@@ -181,9 +191,8 @@
     spectrumColor = [StringToColor([[NSUserDefaults standardUserDefaults] objectForKey:@"SpectrumColor"]) retain];
     waterfallColor = [StringToColor([[NSUserDefaults standardUserDefaults] objectForKey:@"WaterfallColor"]) retain];
     cursorColor = [StringToColor([[NSUserDefaults standardUserDefaults] objectForKey:@"CursorColor"]) retain];
-
-    [self setNeedsDisplay:YES];
-    return self;
+    
+    [self setNeedsDisplay: YES];
 }
 
 @end
