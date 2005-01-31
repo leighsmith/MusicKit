98cae1f3cf32b83b0bfa305b7f093a59b7cbb860
Leigh Smith <leigh@leighsmith.com>
2005-01-30 19:00:38 -0500

    Corrected typing of ivars and methods. Removed redundant ivars

--- MusicKit/Applications/Spectro/WaterfallView.h
+++ MusicKit/Applications/Spectro/WaterfallView.h
@@ -1,11 +1,22 @@
-/* WaterfallView.h -- Interface for WaterfallView class */
+/* 
+ * $Id$ 
+ *
+ * Modifications Copyright (c) 2005 The MusicKit Project, All Rights Reserved.
+ *
+ * Legal Statement Covering Additions by The MusicKit Project:
+ *
+ *   Permission is granted to use and modify this code for commercial and
+ *   non-commercial purposes so long as the author attribution and copyright
+ *   messages remain intact and accompany all relevant code.
+ *
+ */
 
 #import <AppKit/AppKit.h>
 
-@interface WaterfallView:NSView
+@interface WaterfallView: NSView
 {
     id delegate;
-    id frameHeight;
+    IBOutlet id frameHeight;
     NSColor *spectrumColor;
     NSColor *cursorColor;
     NSColor *waterfallColor;
@@ -13,24 +24,21 @@
     int length;
     int totalFrames;			/* number of spectral frames to display */
     int currentFrame;
-    int lastFrame;
     double dataFactor;			/* Number of data points per pixel */
     double deltaX;
     double deltaY;
     double yNorm;
-    NSBezierPath **waterFallPath;	/* array of bezier paths, one per frame */
+    NSBezierPath **waterFallPaths;	/* array of bezier paths, one per frame */
     int cursorPixel;			/* Cursor location (as pixel column) */
-    BOOL draw;
-    BOOL placeSliderLine;
 }
 
-- initWithFrame:(NSRect)theFrame;
-- (void)setDelegate:(id)anObject;
+- initWithFrame: (NSRect) theFrame;
+- (void) setDelegate: (id) anObject;
 - delegate;
-- setup:(int)numFrames length:(int)numPoints;
-- storeNext:(float *)f;
-- (void)drawRect:(NSRect)rects;
-- placeSliderLineAt:(int)frameNum;
-- setColors;
+- setup: (int) numFrames length: (int) numPoints;
+- storeNext: (float *) f;
+- (void) drawRect: (NSRect) rects;
+- (void) placeSliderLineAt: (int) frameNum;
+- (void) setColors;
 
 @end
