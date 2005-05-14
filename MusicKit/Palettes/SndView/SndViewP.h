7bb5317ef31bc633c674d68ea4de626447b6ea6c
Leigh Smith <leigh@leighsmith.com>
2005-05-14 01:48:16 -0500

    Added headers

--- MusicKit/Palettes/SndView/SndViewP.h
+++ MusicKit/Palettes/SndView/SndViewP.h
@@ -1,3 +1,18 @@
+//
+//  $Id$
+//
+//  Description:
+//    Creates three prototype SndViews with different display modes and scrolling.
+//
+//  Original Author: Stephen Brandon
+//
+//  Copyright (c) 2000, The MusicKit Project.  All rights reserved.
+//
+//  Permission is granted to use and modify this code for commercial and
+//  non-commercial purposes so long as the author attribution and copyright
+//  messages remain intact and accompany all relevant code.
+//
+
 #import <InterfaceBuilder/InterfaceBuilder.h>
 #import <SndKit/SndKit.h>
 
