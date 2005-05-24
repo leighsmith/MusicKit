5a481967cee87fef2b2412c8983689d6e4724794
Leigh Smith <leigh@leighsmith.com>
2005-05-24 01:31:13 -0500

    Added doxygen comments of enum fields from RTF doco

--- MusicKit/Frameworks/MusicKit/timeunits.h
+++ MusicKit/Frameworks/MusicKit/timeunits.h
@@ -2,28 +2,28 @@
   $Id$
   Defined In: The MusicKit
 
-  Description:
   Original Author: David Jaffe
 
   Copyright (c) 1988-1992, NeXT Computer, Inc.
   Portions Copyright (c) 1994 NeXT Computer, Inc. and reproduced under license from NeXT
   Portions Copyright (c) 1994 Stanford University
-*/
-/*
-Modification history:
-
-  $Log$
-  Revision 1.2  1999/07/29 01:26:18  leigh
-  Added Win32 compatibility, CVS logs, SBs changes
-
+  Portions Copyright (c) 1999-2005 The MusicKit Project.
 */
 #ifndef __MK_timeunits_H___
 #define __MK_timeunits_H___
 
-/* Time units used for writing files */
+/*!
+  @brief This enumeration specifies the manner in which time is recorded by
+  MKScorefileWriter, MKPartWriter and MKScoreWriter classes.  The values are: 
+ */
 typedef enum _MKTimeUnit {
+    /*!  Time is stored in beats. */
     MK_beat,
+    /*! Time is stored in seconds.  */
     MK_second,
+    /*!  The time is taken from the timeTag in the MKNote itself.
+    Care must be taken when using this TimeUnit - otherwise, out-of-order MKNotes may result.
+    */
     MK_timeTag
 } MKTimeUnit;
 
